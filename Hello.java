d21b2b632e7eb771116010fb54964656512a32cc
RomShemer <romshemer2000@gmail.com>
2025-04-14 15:29:44 +0300

    Test SCM polling

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
   public static void main(String[] args) {
       for(int i=1; i<=10; i++){
-	        System.out.println("This is an updated message from Hello.java!");
+	        System.out.println("Checking if polling works!");
 	  }
   }
 }
